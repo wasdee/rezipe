8b8f35c67bd422dc911b517144918830cf2cca09
Nutchanon Ninyawee <nutchanon@codustry.com>
2021-04-11 07:23:10 +0700

    add pizza

---
+++ interface design/pan_pizza.py
@@ -0,0 +1,75 @@
+# # For the dough
+
+# 1 cup (120g) bread flour + more for kneading (all-purpose is fine instead)
+# 1/2 teaspoon (3g) salt
+# 1/2 teaspoon (2-3g) sugar
+# 1/2 teaspoon (1-2g) dried yeast
+# 1/4 teaspoon (1g) garlic powder (very optional)
+# 2 teaspoons (10mL) olive oil
+# 1/2 cup (120mL) milk (water is fine instead)
+
+# # For the sauce
+# 1/3 cup (80mL) canned crushed tomatoes (I like Pastene "Kitchen Ready")
+# 1 teaspoon (5g) tomato paste (I only use this when I can't get the Pastene tomatoes)
+# a small pinch of sugar
+# a few pinches of dried pizza herbs (oregano, basil, marjoram, chili flakes, etc)
+
+# # For the rest
+# 5-6 oz (140-170g) low-moisture mozzarella (ideally not pre-grated)
+# grated parmesan
+# additional olive oil and pizza herbs
+
+# Combine all the dough ingredients. If it's too wet to knead, add more flour, but try to keep the dough as sticky as you can handle. Knead until smooth and elastic. Cover and let it double in size, 1-2 hours. (You can also simply throw it in the refrigerator and let it rise slowly for 1-7 days.)
+
+# Get a 10-inch (25cm) non-stick skillet (you can use well-seasoned cast iron instead) and drop in just enough olive oil to coat the bottom. Put in some of your pizza herbs and a tiny pinch of salt, and coat the pan with oil and seasonings. Transfer your risen dough ball to the pan and flatten it into the pan with your finger tips, being sure to stretch it a little wider than you want it (it'll contract a bit). Rub the surface with a little more olive oil and let it rise again in the pan for at least 30 min.
+
+# Combine all the sauce ingredients and grate your cheese. Position an oven rack up close to the broiler (grill).
+
+# When you're ready to bake, turn a burner on medium heat under your dough, and get the broiler in your oven heating to maximum. Cook the dough on the stovetop until the bottom is browned to your liking, checking frequently by lifting it up with a soft spatula. You can shake the pan to make sure the dough isn't sticking. I like the bottom to be just starting to burn — this usually takes me 6-7 minutes.
+
+# When the bottom is cooked, take the pan off the heat. Spread on the sauce, edge to edge. Dust the sauce with parmesan, and spread on the mozzarella, edge to edge. Put on any other toppings you want. 
+
+# Transfer the pan under the hot broiler, close the oven door, and cook until the cheese is brown to your liking. This usually takes me only two minutes, which in my experience isn't enough time to overheat the teflon pan and cause the release of hazardous fumes. If you need to be particularly cautious about such things (pet birds and people with respiratory diseases are at high risk), use a cast iron pan instead for this recipe.
+
+# Remove the pizza from the oven and allow it to cool just until it's solid enough that you can slide it out intact — no more than 5 minutes. (The sooner you get it out of the pan, the crispier the bottom will be.) Slide the pizza out to a cooling rack, and let cool to eating temperature. If the bottom is too soft or soggy, you can slide it back into the pan and fry over medium heat again for a minute until sizzling. 
+
+# Slice and eat (duh).
+
+# ref https://www.youtube.com/watch?v=9TjUWnAK0cg
+
+
+
+
+# https://www.youtube.com/watch?v=uYxB4QBlrx4
+
+# ****RECIPE****
+
+# 10-inch Cast Iron Pan Pizza
+# Serves two — if you want to feed more people, you need more pans
+
+# 1/4 teaspoon active dry yeast
+# 1 teaspoon sugar
+# 1/2 cup warm water
+# 1-1.5 cups flour (bread or all purpose)
+# 1/2 teaspoon salt
+# olive oil
+# cornmeal
+# oregano
+# additional salt
+# pepper
+# 1/3 cup canned pureed tomatoes (I like Pastene "Kitchen Ready" ground tomatoes)
+# additional sugar
+# fresh basil
+# grated parmesan
+# 4 ounces whole milk, low moisture mozzarella, grated and kept cold
+# 1 jalapeño pepper (optional), thinly sliced
+
+# Mix the yeast, sugar and warm water together in a bowl and let sit for 5 minuets. If the yeast float to the top and start to go foamy, that means they're alive and you're good to proceed. Put in one cup of flour and the 1/2 teaspoon of salt, mix until it comes together, flour your hands and start kneading. Keep adding flour as needed to keep the dough from being untenably sticky, but don't add too much; it should be pretty wet and sticky at the end. Knead until you can stretch it out thin without it tearing. Form the dough into a smooth ball, coat it and the inside of the bowl with olive oil, cover, and let rise — either for at least 24 hours in the fridge, or 1-2 hours at room temperature — until doubled in size.
+
+# In a 10-inch cast iron pan (or 10-inch Teflon pan), pour in enough olive oil to coat the bottom well. Put in a pinch of cornmeal, a little oregano, lots of black pepper and a small pinch of salt. Mix that together with your fingers and spread the seasoned oil around the bottom, corners and edges of the pan. Before you wash your hands, grab the risen dough ball and stretch it out a little wider than the pan. Put it in the pan, and let ir proof in there for a half hour, until puffy.
+
+# For the sauce, combine the tomatoes with a glug of olive oil, a pinch of sugar, and a few torn leaves of fresh basil. Make sure your cheese is ready and kept cold in the fridge.
+
+# Put the pan on your largest burner. Turn the burner on medium and your oven's broiler (grill) on high. Spoon the sauce onto the dough and spread it edge to edge. After the heat has been on for about 5 minutes, sprinkle the pizza with parmesan and then the mozzarella, edge to edge. Put on the jalapeño slices (or any other toppings, or not), and get ready to transfer the pizza to the oven. It's hard to tell when the right moment is — in my cast iron pan, 7-8 minutes from the time I turn on the burner is the perfect time, but it takes practice. The best indicator may be your nose — the second you smell something starting to burn, move the pizza. Broil it on a high rack until the top is brown to your liking, 4-5 minutes.
+
+# Remove the pizza and let it cool in the pan until firm. Use a butter knife to release the rim of browned cheese from the pan, and then pull the pizza out with tongs. The bottom may stick a bit — you can either try to scrape under there to release it, or simple tear it off with the tongs, it should be solid enough after cooling down to come out intact.
