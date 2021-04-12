4756809af10867afa144f5fdb098653690a8a0bf
Nutchanon Ninyawee <nutchanon@codustry.com>
2021-04-12 09:17:57 +0700

    add bbq rib

---
+++ interface design/bbq_rib.py
@@ -0,0 +1,44 @@
+# https://www.youtube.com/watch?v=yUfjRXnurNk
+# https://www.ethanchlebowski.com/cooking-techniques-recipes/oven-ribs-smoked-vs-liquid-smoke
+
+# Peppery & Sweet Barbecue Sauce
+
+# 1/4 onion
+
+# 1 garlic clove
+
+# A drizzle of neutral oil (peanut, vegetable, canola)
+
+# 1 g Chile powder
+
+# Salt to taste
+
+# 30 cranks Black Pepper
+
+# 30 g Brown sugar
+
+# 150 g Ketchup
+
+# 15 g Mustard
+
+# 15 g white vinegar
+
+# 15 g Worcestershire sauce
+
+# Juice from 1/4 lemon
+
+# 20 g honey
+
+# 5 g hot sauce
+
+# Method:
+
+# Very finely mince the onion and garlic clove so it melts away when cooked. Drizzle some oil in a saucepan over medium heat. Cook the onion and garlic until translucent.
+
+# Add the chile powder, salt, black pepper, brown sugar, ketchup, mustard, vinegar, Worcestershire sauce, lemon juice, honey, and hot sauce to a bowl and whisk to combine.
+
+# Pour the bowl of sauce into the saucepan and simmer, stirring occasionally, until the sauce thickens, about 10 minutes.
+
+# Taste it. Adjust the seasonings as you see fit. Want it more acidic? Add lemon or vinegar. Sweeter? Add sugar. Needs more flavor overall? Add more salt.
+
+# Store in an airtight jar in the fridge for 4-6 months.
